f1bc3a3d4f9ca1dc2f434412f699b0a3448b4d6e
Jose Antonio Carneiro Avila <jose.carneiro.avila@gmail.com>
2023-08-02 18:55:12 -0300

    improvindo One letter case

--- TDD-study/src/main/java/com/zezesheep/TDDstudy/RomanNumbers/RomanNumberConverter.java
+++ TDD-study/src/main/java/com/zezesheep/TDDstudy/RomanNumbers/RomanNumberConverter.java
@@ -1,11 +1,23 @@
 package com.zezesheep.TDDstudy.RomanNumbers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RomanNumberConverter {
 
+    private static Map<String, Integer> table = 
+        new HashMap<>(){{
+            put("I", 1);
+            put("V", 5);
+            put("X", 10);
+            put("L", 50);
+            put("C", 100);
+            put("D", 500);
+            put("M", 1000);
+        }};
+
     public int convert(String numberInRoman) {
-        if(numberInRoman.equals("I")) return 1;
-        else if(numberInRoman.equals("V")) return 5;
-        return 0;
+        return table.get(numberInRoman);
     }
     
 }
