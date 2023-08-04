50bf3a993a44a1e147555adecdc595974ca9f287
Jose Antonio Carneiro Avila <jose.carneiro.avila@gmail.com>
2023-08-04 20:38:07 -0300

    easiest way

---
+++ TDD-study/src/main/java/com/zezesheep/TDDstudy/SalaryManagement/SalaryCalculator.java
@@ -0,0 +1,9 @@
+package com.zezesheep.TDDstudy.SalaryManagement;
+
+public class SalaryCalculator {
+
+    public double executeSalaryCalculus(Worker developer) {
+        return 1350.0;
+    }
+    
+}
