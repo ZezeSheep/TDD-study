fc157fc7e779fce702c09bb39f710ecd567ce9c8
Jose Antonio Carneiro Avila <jose.carneiro.avila@gmail.com>
2023-08-05 10:21:58 -0300

    improving the test in a generalized way

--- TDD-study/src/main/java/com/zezesheep/TDDstudy/SalaryManagement/SalaryCalculator.java
+++ TDD-study/src/main/java/com/zezesheep/TDDstudy/SalaryManagement/SalaryCalculator.java
@@ -2,8 +2,13 @@ package com.zezesheep.TDDstudy.SalaryManagement;
 
 public class SalaryCalculator {
 
-    public double executeSalaryCalculus(Worker developer) {
-        return developer.getSalary() * 0.9;
+    public double executeSalaryCalculus(Worker worker) {
+        if(worker.getSalary() > 3000.0){
+            return worker.getSalary() * 0.8;    
+        }
+        else{
+            return worker.getSalary() * 0.9;
+        }
     }
     
 }
