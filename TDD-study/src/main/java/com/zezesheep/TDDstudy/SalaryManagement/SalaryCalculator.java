8a4f74e34b41ae31c667fd918c777f9d42a15591
Jose Antonio Carneiro Avila <jose.carneiro.avila@gmail.com>
2023-08-05 10:19:32 -0300

    refactoring function into a better way

--- TDD-study/src/main/java/com/zezesheep/TDDstudy/SalaryManagement/SalaryCalculator.java
+++ TDD-study/src/main/java/com/zezesheep/TDDstudy/SalaryManagement/SalaryCalculator.java
@@ -3,11 +3,7 @@ package com.zezesheep.TDDstudy.SalaryManagement;
 public class SalaryCalculator {
 
     public double executeSalaryCalculus(Worker developer) {
-        if(developer.getRole() == Role.DEVELOPER){
-            if(developer.getSalary() > 3000.0) return 3200.0;
-            return 1350.0;
-        }
-        return 425.0;
+        return developer.getSalary() * 0.9;
     }
     
 }
