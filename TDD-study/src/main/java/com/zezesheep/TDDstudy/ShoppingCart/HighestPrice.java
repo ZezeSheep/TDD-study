b3d7bccb32060cd4524a481e65fbfb1443d2ba5f
Jose Antonio Carneiro Avila <jose.carneiro.avila@gmail.com>
2023-08-10 06:45:26 -0300

    one more case

--- TDD-study/src/main/java/com/zezesheep/TDDstudy/ShoppingCart/HighestPrice.java
+++ TDD-study/src/main/java/com/zezesheep/TDDstudy/ShoppingCart/HighestPrice.java
@@ -3,7 +3,15 @@ package com.zezesheep.TDDstudy.ShoppingCart;
 public class HighestPrice {
 
     public double find(ShoppingCart shoppingCart){
-        return 0;
+        if(shoppingCart.getItems().size() == 0) return 0;
+
+        double maior = shoppingCart.getItems().get(0).getTotalValue();
+        for(ShoppingItem item : shoppingCart.getItems()){
+            if(maior < item.getTotalValue()){
+                maior = item.getTotalValue();
+            }
+        }
+        return maior;
     }
     
 }
--- TDD-study/src/test/java/com/zezesheep/TDDstudy/ShoppingCart/HighestPriceTest.java
+++ TDD-study/src/test/java/com/zezesheep/TDDstudy/ShoppingCart/HighestPriceTest.java
@@ -29,5 +29,20 @@ public class HighestPriceTest {
         
         assertEquals(900.0, value, 0.0001);
     }
+
+    @Test
+    public void mustReturnValueIfCartWithLotsOfItems(){
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addItem(new ShoppingItem("Freezer", 1, 900.0));
+        shoppingCart.addItem(new ShoppingItem("Stove", 1, 1500.0));
+        shoppingCart.addItem(new ShoppingItem("Washing Machine", 1, 750.0));
+
+        HighestPrice algorithm = new HighestPrice();
+
+        double value = algorithm.find(shoppingCart);
+        
+        assertEquals(1500.0, value, 0.0001);
+    }
     
 }
