4fa4d0abb18ed7ce5ff6f9f66bf8ee1b0aab1392
Jose Antonio Carneiro Avila <jose.carneiro.avila@gmail.com>
2023-08-10 06:39:28 -0300

    new case test

---
+++ TDD-study/src/main/java/com/zezesheep/TDDstudy/ShoppingCart/HighestPrice.java
@@ -0,0 +1,9 @@
+package com.zezesheep.TDDstudy.ShoppingCart;
+
+public class HighestPrice {
+
+    public double find(ShoppingCart shoppingCart){
+        return 0;
+    }
+    
+}
--- TDD-study/src/test/java/com/zezesheep/TDDstudy/ShoppingCart/HighestPriceTest.java
+++ TDD-study/src/test/java/com/zezesheep/TDDstudy/ShoppingCart/HighestPriceTest.java
@@ -16,5 +16,18 @@ public class HighestPriceTest {
         
         assertEquals(0.0, value, 0.0001);
     }
+
+    @Test
+    public void mustReturnValueOfItemIfCartWithOneElement(){
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addItem(new ShoppingItem("Freezer", 1, 900.0));
+
+        HighestPrice algorithm = new HighestPrice();
+
+        double value = algorithm.find(shoppingCart);
+        
+        assertEquals(900.0, value, 0.0001);
+    }
     
 }
