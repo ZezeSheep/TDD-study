dda5868b7c4df0c05c23be0fa24d6f9eee58e22a
Jose Antonio Carneiro Avila <jose.carneiro.avila@gmail.com>
2023-08-19 17:27:20 -0300

    a first refactor

--- TDD-study/src/main/java/com/zezesheep/TDDstudy/SalaryManagement/SalaryCalculator.java
+++ TDD-study/src/main/java/com/zezesheep/TDDstudy/SalaryManagement/SalaryCalculator.java
@@ -4,23 +4,31 @@ public class SalaryCalculator {
 
     public double executeSalaryCalculus(Worker worker) {
         if(worker.getRole().equals(Role.DEVELOPER)){
-            if(worker.getSalary() > 3000.0){
-                return worker.getSalary() * 0.8;    
-            }
-            else{
-                return worker.getSalary() * 0.9;
-            }
+            return tenOrTwelvePercentDiscount(worker);
         }
         else if(worker.getRole().equals(Role.DBA) || worker.getRole().equals(Role.TESTER)){
-            if(worker.getSalary() < 2500.0){
-                return worker.getSalary() * 0.85;    
-            }
-            else{
-                return worker.getSalary() * 0.75;
-            }
+            return fifteenOrTweentyFivePercentDicount(worker);
         }
         else{
             throw new RuntimeException("Invalid worker");
+        }
+    }
+
+    private double tenOrTwelvePercentDiscount(Worker worker) {
+        if(worker.getSalary() > 3000.0){
+            return worker.getSalary() * 0.8;    
+        }
+        else{
+            return worker.getSalary() * 0.9;
+        }
+    }
+
+    private double fifteenOrTweentyFivePercentDicount(Worker worker) {
+        if(worker.getSalary() < 2500.0){
+            return worker.getSalary() * 0.85;    
+        }
+        else{
+            return worker.getSalary() * 0.75;
         }
     }
     
