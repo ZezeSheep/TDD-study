4b3ea647dc714cd91b19eff0ab52d1393f3db2d7
Jose Antonio Carneiro Avila <jose.carneiro.avila@gmail.com>
2023-08-26 18:51:07 -0300

    one more case in BillProcessor

--- TDD-study/src/main/java/com/zezesheep/TDDstudy/BillProcessor/BillProcessor.java
+++ TDD-study/src/main/java/com/zezesheep/TDDstudy/BillProcessor/BillProcessor.java
@@ -5,9 +5,16 @@ import java.util.List;
 public class BillProcessor {
 
     public void process(List<Bill> bills, Invoice invoice) {
-        Bill bill = bills.get(0);
-        Payment payment = new Payment(bill.getValue(), PaymentWay.BILL);
-        invoice.getPayments().add(payment);
+        double totalPayed = 0;
+        for(Bill bill : bills){
+            Payment payment = new Payment(bill.getValue(), PaymentWay.BILL);
+            invoice.getPayments().add(payment);
+            totalPayed += bill.getValue(); 
+        }
+
+        if(totalPayed >= invoice.getValue()){
+            invoice.setPayed(true);
+        }
     }
     
 }
--- TDD-study/src/test/java/com/zezesheep/TDDstudy/BillProcessor/BillProcessorTest.java
+++ TDD-study/src/test/java/com/zezesheep/TDDstudy/BillProcessor/BillProcessorTest.java
@@ -1,6 +1,7 @@
 package com.zezesheep.TDDstudy.BillProcessor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +24,40 @@ public class BillProcessorTest {
 
         assertEquals(1, invoice.getPayments().size());
         assertEquals(150.0, invoice.getPayments().get(0).getValue(), 0.00001);
+    }
+
+    @Test
+    public void mustProcessPaymentBySeveralBills(){
+        BillProcessor billProcessor = new BillProcessor();
+
+        Invoice invoice = new Invoice("Client", 150.0);
+        Bill b1 = new Bill(150.0);
+        Bill b2 = new Bill(200.0);
+        List<Bill> bills = new ArrayList<>();
+        bills.add(b1);
+        bills.add(b2);
+
+        billProcessor.process(bills, invoice);
+
+
+        assertEquals(2, invoice.getPayments().size());
+        assertEquals(150.0, invoice.getPayments().get(0).getValue(), 0.00001);
+        assertEquals(200.0, invoice.getPayments().get(1).getValue(), 0.00001);
+    }
+
+    @Test
+    public void mustMarkInvoiceAsPaidIfSingleBillPaysEverything(){
+        BillProcessor billProcessor = new BillProcessor();
+
+        Invoice invoice = new Invoice("Client", 150.0);
+        Bill b1 = new Bill(150.0);
+        List<Bill> bills = new ArrayList<>();
+        bills.add(b1);
+
+        billProcessor.process(bills, invoice);
+
+
+        assertTrue(invoice.isPayed());
     }
     
 }
